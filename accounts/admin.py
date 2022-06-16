11453accf0f839dbc7be3e3b5949d5b8e9dbe87b
tobi.akinbiyi@aptissio.com <tobi.akinbiyi@aptissio.com>
2022-06-16 11:32:19 +1000

    initial Commit for BCH Directory

---
+++ accounts/admin.py
@@ -0,0 +1,25 @@
+import imp
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserCreationForm
+    model = CustomUser
+    list_display = [
+        "username",
+        "email",
+        "is_superuser",
+    ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+from dataclasses import fields
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "email",
+        )
+        # help_text = {"email": None, "username": None}
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "email",
+        )
